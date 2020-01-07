fbeb54b40c3d316127fcbceb4f737455e672c157
triton-base <botmayank@gmail.com>
2020-01-07 17:15:52 +0200

    Minimal example using integer

--- src/main.cpp
+++ src/main.cpp
@@ -10,33 +10,41 @@
 #include <chrono>
 #include <thread>
 #include <mutex>
+#include <unistd.h>
 
-std::map<std::string, std::string> g_pages;
-std::mutex g_pages_mutex;
+std::mutex _mutex;
+int shared_val = 99;
 
-void save_page(const std::string &url)
-{
-    // simulate a long page fetch
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    std::string result = "fake content";
+void set_100() {
+        int count = 50;
+        while(count--) {
+            std::unique_lock<std::mutex> writeLock(_mutex);
+            shared_val = 100;
+            sleep(0.5);
+        }
+}
 
-    std::stringstream ss;
-    ss << result << ": " << std::this_thread::get_id();
-    
-    std::lock_guard<std::mutex> guard(g_pages_mutex);
-    g_pages[url] = ss.str();
+void set_50() {
+        int count = 150;
+
+        while(count--) {
+            std::unique_lock<std::mutex> writeLock(_mutex);
+            shared_val = 50;
+            sleep(0.5);
+        }
 }
 
 int main() {
     std::cout << "Testing thread sync primitives" << std::endl;
+    std::thread t1(set_100);
+    std::thread t2(set_50);
 
-    std::thread t1(save_page, "http://foo");
-    std::thread t2(save_page, "http://bar");
+    // std::cin.get();
+
     t1.join();
     t2.join();
- 
-    // safe to access g_pages without lock now, as the threads are joined
-    for (const auto &pair : g_pages) {
-        std::cout << pair.first << " => " << pair.second << '\n';
-    }
+
+    std::cout << "Value of shared val: " << shared_val << std::endl;
+
+    return 0;
 }
